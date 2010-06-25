0e4904dd94d13e44c331aca643ebb36709b0ca66
Andrew Short <andrewjshort@gmail.com>
2010-06-25 16:36:49 +1000

    Fixed indenting in the config file.

--- _config.php
+++ _config.php
@@ -9,16 +9,16 @@ Object::add_extension('SiteTree', 'InlineHelpExtension');
  * Set up a simplified HTML editor config for use in help text.
  */
 HtmlEditorConfig::get('simple')->setOptions(array(
-        'friendly_name'	    => 'Simple',
-        'language'          => i18n::get_tinymce_lang(),
-        'document_base_url' => Director::absoluteBaseURL(),
-        'mode'              => 'specific_textareas',
-        'valid_elements'    => '*[*]'
+	'friendly_name'	    => 'Simple',
+	'language'          => i18n::get_tinymce_lang(),
+	'document_base_url' => Director::absoluteBaseURL(),
+	'mode'              => 'specific_textareas',
+	'valid_elements'    => '*[*]'
 ));
 HtmlEditorConfig::get('simple')->setButtonsForLine(1, array(
-        'bold', 'italic', 'underline', 'strikethrough', 'separator', 'undo',
-        'redo', 'separator', 'formatselect', 'cleanup', 'separator',
-        'bullist', 'numlist'
+	'bold', 'italic', 'underline', 'strikethrough', 'separator', 'undo',
+	'redo', 'separator', 'formatselect', 'cleanup', 'separator',
+	'bullist', 'numlist'
 ));
 HtmlEditorConfig::get('simple')->setButtonsForLine(2);
 HtmlEditorConfig::get('simple')->setButtonsForLine(3);
